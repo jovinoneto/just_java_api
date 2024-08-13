5117a6bd6a3b8f8ad4effd356610e9537ac54cf3
jovinoneto <jovinone@gmail.com>
2024-08-13 18:14:10 -0300

    first commit

---
+++ src/main/java/br/com/usatec/just_java_api/JustJavaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.usatec.just_java_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustJavaApiApplication.class, args);
+	}
+
+}
